0f559b77cc4795ea579a89e52456343fb7a2b3e4
Samuel-Levesque <samuel-levesque@hotmail.com>
2018-11-14 20:31:56 -0500

    Added sample pickle logic to be used to avoid running same model multiple times.

---
+++ src/k_nearest_neighbors.py
@@ -0,0 +1,8 @@
+
+def fit_knn(features, labels):
+    """
+    Returns the fitted KNN models to be used in the rest of the analysis
+    :param features: features to be used for model fitting
+    :param labels: Target variable to use for fitting parameter
+    :return models:
+    """
--- src/main.py
+++ src/main.py
@@ -8,6 +8,9 @@ Created on Wed Nov 14 19:35:36 2018
 
 from run_config import run_config
 from import_data import import_raw_data
+from k_nearest_neighbors import fit_knn
+
+import pickle
 
 
 def main():
@@ -24,7 +27,11 @@ def main():
         pass
 
     if run_config["fit_knn"]:
-        pass
+        knn_model = fit_knn(raw_data["coveo_clicks_train"],
+                            raw_data["coveo_searches_train"])
+        pickle.dump(knn_model, open("knn_model", "wb"))
+
+    knn_model = pickle.load(open("knn_model", "rb"))
 
     if run_config["fit_naive_bayes"]:
         pass
