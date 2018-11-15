7533d492741ab3e9b6ec8b1e97fab91503da6ef4
Samuel-Levesque <samuel-levesque@hotmail.com>
2018-11-14 19:47:44 -0500

    Ajout de fichiers pour structure du squelette de code

--- src/import-data.py
+++ src/import-data.py
@@ -7,8 +7,11 @@ Created on Thu Nov  1 06:46:01 2018
 """
 
 import pandas
+import numpy as np
 
-coveo_clicks_train = pandas.read_csv('data/coveo_clicks_train.csv')
+X_full_data = np.genfromtxt("../data/coveo_clicks_train.csv", delimiter=",", skip_header=True)
+
+coveo_clicks_train = pandas.read_csv(r"../data/coveo_clicks_train.csv")
 coveo_clicks_valid = pandas.read_csv('data/coveo_clicks_valid.csv')
 coveo_searches_train = pandas.read_csv('data/coveo_searches_train.csv')
 coveo_searches_valid = pandas.read_csv('data/coveo_searches_valid.csv')
---
+++ src/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 14 19:35:36 2018
+
+@author: Samuel_Levesque
+"""
+
+import numpy as np
+import pandas as pd
+
+def main():
+    np.genfromtxt(r"../data/coveo_clicks_train.csv")
+
+
+if __name__ == "__main__":
+    main()
